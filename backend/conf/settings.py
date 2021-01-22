1fd6b203be46beb047e365d23d55cc35e28512ad
Rafal Maszlej <h4stoor@gmail.com>
2021-01-22 14:33:18 +0100

    Postgres docker container

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 python manage.py collectstatic --noinput
+./scripts/db_health_check.sh
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
